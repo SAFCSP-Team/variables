74c792bb2ae0e8ef9a76e5f7027b2ceabb2edd4e
sarialqahtani0 <sarialqahtani527@gmail.com>
2026-02-02 19:05:47 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-      /* add your code here */
-    }
+        float myVariable = 2.5f;
 
+        System.out.println(myVariable);
+
+        myVariable = 5.5f;
+
+        System.out.println(myVariable);
+
+        int newValue = (int) myVariable;
+
+        System.out.println(newValue);
+    }
 }
